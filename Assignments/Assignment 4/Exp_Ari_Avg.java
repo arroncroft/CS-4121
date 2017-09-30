ddc8e12ac94285edc95fffbc64cbaec584ce7f06
arroncroft <arroncroft@hotmail.com>
2017-09-30 19:45:47 -0400

    made myData

--- Assignments/Assignment 4/Exp_Ari_Avg.java
+++ Assignments/Assignment 4/Exp_Ari_Avg.java
@@ -9,38 +9,36 @@ import java.io.IOException;
 import java.util.Random;
 
 public class Exp_Ari_Avg{  	 
-	 public static double expAvg(double preExpAvg, int newSample){
-	 	// To be completed...
-	 	return 0;
-	 }
+   public static double expAvg(double preExpAvg, int newSample){
+    	// To be completed...
+      return 0;
+   }
 	 
-	 public static double ariAvg(double preAriAvg, int newSample, int n){
-		// To be completed...
-	 	return 0;
-	 }
+   public static double ariAvg(double preAriAvg, int newSample, int n){
+   	// To be completed...
+      return 0;
+   }
 	 
-	 public static void sampleRTT2EstimatedRTT(Scanner in, PrintWriter out)throws IOException{ 
-	 	// To be completed...
-	 }
-	 public static void main (String[] args){ 
-	 	try{
-			
-			Scanner fileScanner = new Scanner(new File("data.txt")); 
-			PrintWriter filePrinter = new PrintWriter(new File("output.txt"));
-			
-			filePrinter.print("SampleRTTs\tExpAvgs\tAriAvgs");
-			filePrinter.println();
-			
-			sampleRTT2EstimatedRTT(fileScanner, filePrinter);
-			
-			fileScanner.close();
-			filePrinter.close();
-	
-		}catch(IOException e){
-			System.out.println("Error opening file.");
-			System.exit(0);
-		}
-		
-		
-	 }
-}  
+   public static void sampleRTT2EstimatedRTT(Scanner in, PrintWriter out)throws IOException{ 
+      // To be completed...
+   }
+    
+   public static void main (String[] args){ 
+      try{
+         Scanner fileScanner = new Scanner(new File("data.txt")); 
+         PrintWriter filePrinter = new PrintWriter(new File("output.txt"));
+      	
+         filePrinter.print("SampleRTTs\tExpAvgs\tAriAvgs");
+         filePrinter.println();
+      	
+         sampleRTT2EstimatedRTT(fileScanner, filePrinter);
+      	
+         fileScanner.close();
+         filePrinter.close();
+      }
+      catch(IOException e){
+         System.out.println("Error opening file.");
+         System.exit(0);
+      }
+   }
+}
